a0d82ef20221cacbcdff2049a00af01db8f02542
Anton Dergunov <1549134+anton-dergunov@users.noreply.github.com>
2024-07-16 12:31:06 +0100

    Updated the style of the pages

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -64,8 +64,8 @@ $(document).ready(function(){
     $(".author__urls-wrapper button").toggleClass("open");
   });
 
-  // init smooth scroll, this needs to be slightly more than then fixed masthead height
-  $("a").smoothScroll({offset: -65});
+  // init smooth scroll
+  $("a").smoothScroll({offset: -20});
 
   // add lightbox class to all image links
   $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
